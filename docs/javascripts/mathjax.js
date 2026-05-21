9fed1986ecb7b57d73327a886b24e8bb7c4ffd3f
long_QY <104426184+lala1108@users.noreply.github.com>
2026-05-21 18:55:42 +0800

    Update mathjax.js

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,10 +1,11 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"]],
-    displayMath: [["\\[", "\\]"]]
+    // 把 "$" 加到行内公式的识别符中
+    inlineMath: [['$', '$'], ["\\(", "\\)"]],
+    displayMath: [['$$', '$$'], ["\\[", "\\]"]]
   },
   options: {
-    ignoreHtmlClass: ".*",
-    processHtmlClass: "arithmatex"
+    // 推荐的默认设置，跳过一些不需要渲染代码的标签
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
   }
 };
